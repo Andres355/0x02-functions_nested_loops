55b73e71a53f08a93a449e7b2db393de1918195f
Andres355 <1973@holbertonschool.com>
2020-06-15 22:55:14 -0500

    _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,13 @@
-# include  < unistd.h >
+#include <unistd.h>
 
-/ * *
- * _putchar: escribe el carácter c en stdout
- * @c: el personaje a imprimir
- * *
- * Retorno: en caso de éxito 1.
- * En caso de error, se devuelve -1 y se establece errno de manera adecuada.
- * /
-int  _putchar ( char c)
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-	return ( escribir ( 1 , & c, 1 ));
+	return (write(1, &c, 1));
 }
